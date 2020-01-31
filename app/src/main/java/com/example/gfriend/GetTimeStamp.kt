5dafa56c3b12733ee80ed2d84440461d2db249fe
A A <aa@enoMacBook-Pro.local>
2020-01-31 11:50:12 +0900

    タイムスタンプを書こうとしてる奴

--- app/src/main/java/com/example/gfriend/GetTimeStamp.kt
+++ app/src/main/java/com/example/gfriend/GetTimeStamp.kt
@@ -1,7 +1,12 @@
 package com.example.gfriend
 
 import java.sql.Timestamp
+import java.text.SimpleDateFormat
 
 class GetTimeStamp(){
-
+    fun getTimeStamp():Timestamp{
+        val dateFormatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val date = dateFormatter.parse("2019-03-06 13:28:00")
+        return date as Timestamp
+    }
 }
